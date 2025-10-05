8780544116e5de7dd31996995390405d296f1003
AnzinFelipe <ian.felipe.drunen@gmail.com>
2025-10-05 12:28:25 -0300

    Criado modelo de temas e modelos no admin para o superuser

--- portal/admin.py
+++ portal/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Noticia, Materia, Tema
+
+admin.site.register(Noticia)
+admin.site.register(Materia)
+admin.site.register(Tema)
 
 # Register your models here.
--- portal/models.py
+++ portal/models.py
@@ -1,19 +1,24 @@
 from django.db import models
 
+class Tema(models.Model):
+    tema = models.CharField(max_length = 50)
+
+    def __str__(self):
+        return self.tema
+
 class Noticia(models.Model):
-    titulo = models.CharField(max_length=100)
-    subtitulo = models.CharField(max_length=200)
-    autor = models.CharField(max_length=50)
+    titulo = models.CharField(max_length=100, blank = False)
+    subtitulo = models.CharField(max_length=200, blank = False)
+    autor = models.CharField(max_length=50, blank = False)
+    data = models.DateTimeField("Publicado em: ", auto_now_add = True)
+    tema = models.ForeignKey(Tema, on_delete = models.CASCADE)
 
     def __str__(self):
         return f"{self.titulo}"
-    
-
 
 class Materia(models.Model):
     cabecalho = models.ForeignKey(Noticia, on_delete=models.CASCADE)
     texto = models.CharField(max_length=50000)
-
 
     def __str__(self):
         return f"{self.texto}"
